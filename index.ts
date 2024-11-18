5c7855b77cdca406ec184e7838b1431ae5b905bb
raven <raven@ravendeMacBook-Air.local>
2024-11-18 20:24:35 +0800

    Tmp

--- index.ts
+++ index.ts
@@ -4,19 +4,16 @@ import { createZGServingNetworkBroker } from "@0glabs/0g-serving-broker";
 // const { createZGServingNetworkBroker } = pkg;
 
 async function main() {
-  const provider = new ethers.JsonRpcProvider(
-    "https://0g-json-rpc-public.originstake.com"
-  );
+  const provider = new ethers.JsonRpcProvider("https://evmrpc-testnet.0g.ai");
 
-  // Step 2: Create a wallet with a private key
-  // Replace 'YOUR_PRIVATE_KEY_HERE' with the actual private key but do not hard-code it in production.
+  // Step 1: Create a wallet with a private key
   const privateKey =
     "2F63294033D7629EED58DF4E9F7B71E7448338BD48E1002F84D543EBD0C54349";
   const wallet = new ethers.Wallet(privateKey, provider);
 
   // Step 2: Initialize the broker
   try {
-    const broker = await createZGServingNetworkBroker(wallet);
+    const broker = await createZGServingNetworkBroker(wallet, "./storage.json");
 
     // Step 3: List available services
     console.log("Listing available services...");
@@ -28,13 +25,12 @@ async function main() {
     });
 
     // Step 4: Manage Accounts
-    // Let's assume we are using a dummy provider address for demonstration
-    const providerAddress = "0xProviderAddress";
+    const providerAddress = services[0].provider;
     const initialBalance = "10000"; // Using a big int value for the balance
 
-    console.log("Creating a new account...");
-    await broker.addAccount(providerAddress, initialBalance);
-    console.log("Account created successfully.");
+    // console.log("Creating a new account...");
+    // await broker.addAccount(providerAddress, initialBalance);
+    // console.log("Account created successfully.");
 
     // Deposit funds into the account
     const depositAmount = "5000";
@@ -48,7 +44,7 @@ async function main() {
 
     // Process a request and obtain headers
     console.log("Processing a request...");
-    const headers = broker.requestProcessor.processRequest(
+    const headers = await broker.requestProcessor.processRequest(
       providerAddress,
       serviceName,
       content
